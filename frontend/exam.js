069cb62353be899a47c52ef6ed352a4ce27142d0
PALA TARUN <144416876+PalaTarun41@users.noreply.github.com>
2025-12-19 21:11:34 +0530

    Add files via upload

--- frontend/exam.js
+++ frontend/exam.js
@@ -1,1 +1,13 @@
+let score = 0;
 
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) score += 10;
+});
+
+document.addEventListener("copy", () => score += 15);
+document.addEventListener("paste", () => score += 15);
+
+window.addEventListener("blur", () => score += 10);
+window.addEventListener("beforeunload", () => score += 20);
+
+setInterval(() => console.log("Suspicion Score:", score), 5000);
